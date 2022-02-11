e49302dcb3675e02ae79c3522641a979a8bce826
semicolon <adom.donatas@gmail.com>
2022-02-11 13:39:39 +0200

    chapter 10 - Autowiring Dependencies into a Service

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,11 +2,10 @@
 
 namespace App\Controller;
 
-use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
+use App\Service\MarkdownHelper;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Contracts\Cache\CacheInterface;
 use Twig\Environment;
 
 class QuestionController extends AbstractController
@@ -29,7 +28,7 @@ class QuestionController extends AbstractController
     /**
      * @Route("/questions/{slug}", name="app_question_show")
      */
-    public function show($slug, MarkdownParserInterface $markdownParser, CacheInterface $cache)
+    public function show($slug, MarkdownHelper $markdownHelper)
     {
         $answers = [
             'Make sure your cat is sitting `purrrfectly` still 🤣',
@@ -39,11 +38,7 @@ class QuestionController extends AbstractController
 
         $questionText = "I've been turned into a cat, any thoughts on how to turn back? While I'm **adorable**, I don't really care for cat food.";
 
-        $parsedQuestionText = $cache->get('markdown_' . md5($questionText), function () use ($questionText, $markdownParser) {
-            return $markdownParser->transformMarkdown($questionText);
-        });
-
-        dump($cache);
+        $parsedQuestionText = $markdownHelper->parse($questionText);
 
         return $this->render('question/show.html.twig', [
             'question' => ucwords(str_replace('-', ' ', $slug)),
